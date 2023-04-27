9d1b634801c3cc221e0b9e305d111f77b60b8c46
diegomes <R@fael2906>
2023-04-26 22:20:30 -0300

    add arquivo da arquivos com exercicios

---
+++ Exercicios/ex003.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número'))
+n2 = int(input('Digite outro numero'))
+
+soma = n1 + n2
+print('A soma dos numeros digitados é ', soma)
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
---
+++ Exercicios/ex004.py
@@ -0,0 +1,4 @@
+n = str(input('Digite algo: '))
+
+print(type(n))
+print(n.isupper())
