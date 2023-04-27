9b44dd3a8353fe3d8116511314109699b3f24e4b
diegomes <R@fael2906>
2023-04-26 22:19:57 -0300

    add arquivo da aula 06 sobre tipos primitivos

---
+++ aula006.py
@@ -0,0 +1,15 @@
+# tipos primitimos
+
+# aqui recebe um inteiro do teclado
+n1 = int(input('Digite um número'))
+
+# aqui recebe um float do teclado
+n2 = float(input('Digite um número'))
+
+# aqui recebe um boolean do teclado
+n3 = bool(input('Digite um número'))
+
+# aqui recebe uma srting do teclado
+n4 = str(input('Digite um número'))
+
+
